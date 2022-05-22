f093d299f90eddd690e08e9f12ca459dec7598e3
Felipe Mourão <felipemourao6124@gmai.com>
2022-05-22 18:34:14 -0300

    Inciando estudos com VueJs e Vuetify

---
+++ src/views/TarefasView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+     <v-list
+      flat
+      subheader
+    >
+      <v-subheader>General</v-subheader>
+
+      <v-list-item-group
+        v-model="settings"
+        multiple
+        active-class=""
+      >
+      <Tarefa />
+      <Tarefa />
+      <Tarefa />
+      <Tarefa />
+
+      </v-list-item-group>
+    </v-list>
+  </div>
+</template>
+
+<script>
+import Tarefa from '../components/tarefas/TarefaComp.vue'
+
+  export default {
+    name: 'HomeView',
+
+    components: {
+      Tarefa
+    },
+  }
+</script>
+
